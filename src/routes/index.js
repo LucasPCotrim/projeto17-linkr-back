ea168338581f643ad628dce1271bd264716d9b46
MateusDiniz <mateus.090998@gmail.com>
2022-10-19 17:23:53 -0300

    backend for login with jwt ready and tested

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,10 @@
-import { Router } from 'express';
+import { Router } from "express";
 // import authRouter from './authRouter.js';
+import loginRouter from "./loginRouter.js";
 
 const router = Router();
 
 // router.use(authRouter);
+router.use(loginRouter);
 
 export default router;

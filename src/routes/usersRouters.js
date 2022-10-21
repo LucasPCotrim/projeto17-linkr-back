e0375f0d66ae52eac47194f28118e310afbc97b2
kcandeloni <kcandeloni@gmail.com>
2022-10-21 17:07:34 -0300

    route userPage + refactoring: standardized file

--- src/routes/usersRouters.js
+++ src/routes/usersRouters.js
@@ -2,10 +2,13 @@ import express from 'express';
 import tokenValidation from '../middlewares/authUserMiddleware.js';
 import logout from '../controllers/usersControllers/logoutController.js'
 import getUser from '../controllers/usersControllers/getUserController.js';
+import getPageUser from '../controllers/usersControllers/getPageUserControler.js';
+
 
 const userRouter = express.Router();
 
 userRouter.get('/logout', tokenValidation, logout);
 userRouter.get('/user', tokenValidation, getUser);
+userRouter.get('/user/:id', tokenValidation, getPageUser);
 
 export default userRouter;

b339f82944723bdd3e92be3429a61b1c8eb7cfbd
kcandeloni <kcandeloni@gmail.com>
2022-10-27 02:51:37 -0300

    update query fetch user page

--- src/controllers/usersControllers/getPageUserControler.js
+++ src/controllers/usersControllers/getPageUserControler.js
@@ -1,15 +1,15 @@
 import usersRepository from "../../repositories/usersRepository.js";
 
-
 async function getPageUser(req, res) {
-  const userId = req.params.id;
+  const userId = parseInt(req.params.id) > 0 ? req.params.id : 0;
   const limit = req.query.limit || 20;
+  const offset = req.query.offset || 0;
   try {
-    const PostUser = await usersRepository.getPostByUserId(userId, limit);
-    if (PostUser.rowCount === 0) {
-      return res.sendStatus(404);
+    const PostUser = await usersRepository.getPostByUserId(userId, limit, offset);
+    if (PostUser.rowCount > 0) {
+      return res.send(PostUser.rows).status(200);
     }
-    return res.send(PostUser.rows).status(200);
+    return res.sendStatus(404);
   } catch (err) {
     console.error(err);
     return res.sendStatus(500);

3fe2ac2afd5da1d36ce3cdfa5135689596b13043
kcandeloni <kcandeloni@gmail.com>
2022-10-28 18:34:40 -0300

    fix bug: repository user

--- src/controllers/usersControllers/getUserController.js
+++ src/controllers/usersControllers/getUserController.js
@@ -4,9 +4,6 @@ async function getUser(req, res) {
   const userId = parseInt(req.params.id) > 0 ? req.params.id : res.locals.user.id;
   try {
     const user = await usersRepository.getUserbyId(userId);
-    if (user.rowCount === 0) {
-      return res.sendStatus(404);
-    }
     return res.send(user.rows[0]).status(200);
   } catch (err) {
     console.error(err);
